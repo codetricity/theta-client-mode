fedb25b6342458f81766be1891a9c7f6ba4bf5ae
Craig <craigoda@gmail.com>
2020-05-19 13:54:10 -0700

    updated bash shell scripts

--- bash/client-mode.sh
+++ bash/client-mode.sh
@@ -12,8 +12,8 @@ GET_INFO="${BASE_URL}osc/info"
 # curl --digest --user "${THETA_ID}:${PASSWORD}"  $GET_INFO
 
 
-# curl --digest --user "THETAYL00105377:00105377" -X GET http://192.168.2.101/osc/info
+curl --digest --user "THETAYL00105377:00105377" -X GET http://192.168.2.101/osc/info
 
-# curl --digest --user "THETAYL00105377:00105377" -H "Content-Type:application/json" -X POST http://192.168.2.101/osc/state
+curl --digest --user "THETAYL00105377:00105377" -H "Content-Type:application/json" -X POST http://192.168.2.101/osc/state
 
 curl -d '{"name":"camera.takePicture"}' --digest --user "THETAYL00105377:00105377" -H "Content-Type:application/json; charset=utf-8;" -X POST http://192.168.2.101/osc/commands/execute
